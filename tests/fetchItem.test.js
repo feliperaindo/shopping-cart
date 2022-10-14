7d8e39b95407630b9c620061abb7b2023d3c1f41
Felipe <felipe.raindo@gmail.com>
2022-10-14 17:24:38 -0300

    incia testes da função fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,5 +3,21 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  expect(typeof fetchItem).toBe('function');
+  test('Verifica se `fetchItem` é uma função', () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+  test('Verifica se `fetch` foi chamado ao chamar a função `fetchItem`', async ()=> {
+    expect(await fetchItem('MLB1615760527')).toBeCalled(fetch);
+  });
+  test('Verifica se a função `fetchItem` utiliza o endpoint quando aplicado o parâmetro `MLB1615760527`', async () => {
+    const url = "https://api.mercadolibre.com/items/MLB1615760527";
+    await fetchItem('MLB1615760527')
+    expect(fetch).toBeCalledWith(url);
+  });
+  test('Verifica se o retorno da função `fetchItem` é idêntico ao conteúdo da constante `item`', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  test('Verifica o retorno do erro esperando em caso da chamada da função sem parâmetro', async () => {
+    expect(fetchItem()).resolves.toThrow('You must provide an url');
+  });
 });
