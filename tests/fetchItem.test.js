d64fca62b57e7b2a26090254d0bd31b49607875a
Felipe <felipe.raindo@gmail.com>
2022-10-14 19:29:42 -0300

    resolve testes do jest

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,6 +6,11 @@ describe('2 - Teste a função fetchItem', () => {
   test('Verifica se `fetchItem` é uma função', () => {
     expect(typeof fetchItem).toBe('function')
   });
+  test('Verifica se `fetch` foi chamado quando a função `fetchItem` for executada', async () => {
+    const url = "https://api.mercadolibre.com/items/MLB1615760527";
+    await fetchItem('MLB1615760527')
+    expect(fetch).toBeCalledTimes(1);
+  })
   test('Verifica se a função `fetchItem` utiliza o endpoint quando aplicado o parâmetro `MLB1615760527`', async () => {
     const url = "https://api.mercadolibre.com/items/MLB1615760527";
     await fetchItem('MLB1615760527')
@@ -15,6 +20,6 @@ describe('2 - Teste a função fetchItem', () => {
     expect(await fetchItem('MLB1615760527')).toEqual(item);
   });
   test('Verifica o retorno do erro esperando em caso da chamada da função sem parâmetro', async () => {
-    expect(fetchItem()).resolves.toThrow('You must provide an url');
+    expect(fetchItem()).rejects.toThrow('You must provide an url');
   });
 });
