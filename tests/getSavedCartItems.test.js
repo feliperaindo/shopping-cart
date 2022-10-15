ddb87b0df960d5b055c3a8e851fc8ff978e59f58
Felipe <felipe.raindo@gmail.com>
2022-10-15 01:15:01 -0300

    códigos para teste de local storage finalizados e não testados

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,15 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se `getSavedCartItems` é  uma função', () => {
+    expect(typeof getSavedCartItems).toBe('function');
+  });
+  test('Verifica se `localStorage.setItem` é aplicado, quando a função `saveCartItems` é requerida', () => {
+    getSavedCartItems('cartItem')
+    expect(localStorage.getItem).toBeCalled(1);
+  });
+  test('Verifica se após chamar a função `saveCartItems` localStorage contem chave de nome `cartItem` e valor respectivo, conforme passado por parâmetro', () => {
+    getSavedCartItems('cartItem');
+    expect(localStorage.getItem).toBeCalledWith('cartItem');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se `saveCartItems` é  uma função', () => {
+    expect(typeof saveCartItems).toBe('function');
+  });
+  test('Verifica se `localStorage.setItem` é aplicado, quando a função `saveCartItems` é requerida', () => {
+    saveCartItems('cartItem')
+    expect(localStorage.setItem).toBeCalled(1);
+  });
+  test('Verifica se após chamar a função `saveCartItems` localStorage contem chave de nome `cartItem` e valor respectivo, conforme passado por parâmetro', () => {
+    saveCartItems('cartItem', 'chave de teste');
+    expect(localStorage.setItem).toBeCalledWith('cartItem', 'chave de teste');
+  });
 });
