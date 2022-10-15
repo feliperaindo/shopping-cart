c35e5ec0f0db7affcb986a444fac077b2a617294
Felipe <felipe.raindo@gmail.com>
2022-10-15 00:25:34 -0300

    adiciona elementos ao carrinho

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Esse tipo de comentário que estão antes de todas as funções são chamados de JSdoc,
-// experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições! 
-
-// Fique a vontade para modificar o código já escrito e criar suas próprias funções!
-
-/**
- * Função responsável por criar e retornar qualquer elemento.
+/** Função responsável por criar e retornar qualquer elemento.
  * @param {string} element - Nome do elemento a ser criado.
  * @param {string} className - Classe do elemento.
  * @param {string} innerText - Texto do elemento.
@@ -18,8 +12,7 @@ const createCustomElement = (element, className, innerText, index = 0) => {
   return e;
 };
 
-/**
- * Função responsável por criar e retornar o elemento de imagem do produto.
+/** Função responsável por criar e retornar o elemento de imagem do produto.
  * @param {string} imageSource - URL da imagem.
  * @returns {Element} Elemento de imagem do produto.
  */
@@ -29,14 +22,49 @@ const createProductImageElement = (imageSource, index) => {
   return img;
 };
 
-function cartItemClickListener(element) {
-  const clickedElement = element.target.id;
-  const newElement = clickedElement.match(/(\d+)/)[0];
-  console.log(newElement);
+function cartItemClickListener() {
+  return 0;
 }
 
-/**
- * Função responsável por criar e retornar o elemento do produto.
+/** Função responsável por criar e retornar um item do carrinho.
+ * @param {Object} product - Objeto do produto.
+ * @param {string} product.id - ID do produto.
+ * @param {string} product.title - Título do produto.
+ * @param {string} product.price - Preço do produto.
+ * @returns {Element} Elemento de um item do carrinho.
+ */
+const createCartItemElement = ({ id, title, price }, number) => {
+  const liText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
+  const li = createCustomElement('li', 'cart__item', liText, number);
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+function addItemInCart(productToBuy) {
+  const getBuyList = document.querySelector('#cart__items');
+  getBuyList.appendChild(productToBuy);
+}
+
+const requireClickedItemInfo = async (productClicked) => fetchItem(productClicked);
+
+async function market(itemClicked, number) {
+  const infoItem = await requireClickedItemInfo(itemClicked);
+  const itemToAddInBuyList = createCartItemElement(infoItem, number);
+  addItemInCart(itemToAddInBuyList);
+}
+
+/** Função que recupera o ID do produto passado como parâmetro.
+ * @param {Element} product - Elemento do produto.
+ * @returns {string} ID do produto.
+ */
+function getIdFromProductItem(element) {
+    const clickedElement = element.target.id;
+    const getElementNumber = clickedElement.match(/(\d+)/)[0];
+    const elementToSearch = document.querySelector(`#span-item_id-${getElementNumber}`);
+    market(elementToSearch.innerText, getElementNumber);
+}
+
+/** Função responsável por criar e retornar o elemento do produto.
  * @param {Object} product - Objeto do produto. 
  * @param {string} product.id - ID do produto.
  * @param {string} product.title - Título do produto.
@@ -52,33 +80,11 @@ const createProductItemElement = ({ id, title, thumbnail }, index) => {
 
   const text = 'Adicionar ao carrinho!';
   const listeningButton = createCustomElement('button', 'item__add', text, index);
-  listeningButton.addEventListener('click', cartItemClickListener);
+  listeningButton.addEventListener('click', getIdFromProductItem);
 
   section.appendChild(listeningButton);
 
   return section;
-};
-
-/**
- * Função que recupera o ID do produto passado como parâmetro.
- * @param {Element} product - Elemento do produto.
- * @returns {string} ID do produto.
- */
-const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
-
-/**
- * Função responsável por criar e retornar um item do carrinho.
- * @param {Object} product - Objeto do produto.
- * @param {string} product.id - ID do produto.
- * @param {string} product.title - Título do produto.
- * @param {string} product.price - Preço do produto.
- * @returns {Element} Elemento de um item do carrinho.
- */
-const createCartItemElement = ({ id, title, price }) => {
-  const liText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
-  const li = createCustomElement('li', 'cart__item', liText);
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 };
 
 const populateWebPage = (population) => {
